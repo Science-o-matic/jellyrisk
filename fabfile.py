36678e3c46f653c17dc128f7d5f4d8e560cd5fe4
ygneo <antonio.barcia@gmail.com>
2013-02-17 13:46:18 +0100

    Add fabfile script

---
+++ fabfile.py
@@ -0,0 +1,35 @@
+from fabric.api import *
+
+env.roledefs = {
+    'jellyrisk': ['jellyrisk@jellyrisk.com'],
+    'nginx': ['jellyrisk.com']
+    }
+env.hosts = ['jellyrisk.com']
+env['project_path'] = "~/www/jellyrisk/"
+
+
+@roles('jellyrisk')
+def git_status():
+    with cd(env['project_path']):
+        run('git fetch && git status') 
+
+
+@roles('jellyrisk')
+def pushpull():
+    local("git push origin master")
+    with cd(env['project_path']):
+        run('git pull') 
+
+
+@roles('nginx')
+def reload_nginx():
+    run('sudo /etc/init.d/nginx reload') 
+
+
+@roles('jellyrisk')
+def release():
+    pushpull()
+    with cd(env['project_path']):
+        run('./manage.py migrate') 
+        run('./manage.py collectstatic') 
+    reload_nginx()
