623090fa691152389f1755e388bea9902b111b6e
ygneo <antonio.barcia@gmail.com>
2013-02-25 22:54:45 +0100

    Serving media in local development server

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,3 +10,9 @@ urlpatterns = patterns('',
     url(r'^', include('cms.urls')),
 )
 
+if settings.DEBUG:
+    urlpatterns = patterns('',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    url(r'', include('django.contrib.staticfiles.urls')),
+) + urlpatterns
