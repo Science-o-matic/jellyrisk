6a729d565cd9fcd90e4b80883a7b68f3888f4cbb
ygneo <antonio.barcia@gmail.com>
2014-03-18 12:30:55 +0100

    Add labels to register form and its corresponding locales

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,14 @@
-from registration.forms import RegistrationForm
 from django import forms
+from django.utils.translation import ugettext_lazy as _
+from registration.forms import RegistrationForm
 
 
 class UserProfileRegistrationForm(RegistrationForm):
-    recieve_newsletter = forms.BooleanField(required=False, initial=True)
-    participate_in_contest = forms.BooleanField(required=False)
+    recieve_newsletter = forms.BooleanField(
+        required=False, initial=True,
+        label=_("Recieve newsletter")
+    )
+    participate_in_contest = forms.BooleanField(
+        required=False,
+        label=_("Participate in jellyfish photo contest")
+    )
